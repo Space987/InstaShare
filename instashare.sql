8eb15c57ce10963f87969ebcc9c78eed0d9b8395
Space987 <82058206+Space987@users.noreply.github.com>
2022-10-12 23:36:44 -0400

    Add files via upload

--- instashare.sql
+++ instashare.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 06, 2022 at 02:28 AM
--- Server version: 10.4.24-MariaDB
--- PHP Version: 8.1.6
+-- Generation Time: Oct 13, 2022 at 05:36 AM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 8.1.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -29,7 +29,6 @@ USE `instashare`;
 -- Table structure for table `comment`
 --
 
-DROP TABLE IF EXISTS `comment`;
 CREATE TABLE `comment` (
   `comment_id` int(11) NOT NULL,
   `publication_id` int(11) NOT NULL,
@@ -38,13 +37,22 @@ CREATE TABLE `comment` (
   `date_time` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `comment`
+--
+
+INSERT INTO `comment` (`comment_id`, `publication_id`, `profile_id`, `comment`, `date_time`) VALUES
+(8, 34, 9, 'cool cat', '2022-10-12 22:55:00'),
+(9, 35, 10, 'Man I love pizza!', '2022-10-12 22:56:00'),
+(12, 34, 11, 'cats suck ;(', '2022-10-12 22:59:00'),
+(13, 35, 11, 'I Love Pizza Too!!!!', '2025-10-27 23:25:00');
+
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `profile`
 --
 
-DROP TABLE IF EXISTS `profile`;
 CREATE TABLE `profile` (
   `profile_id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
@@ -53,13 +61,21 @@ CREATE TABLE `profile` (
   `last_name` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `profile`
+--
+
+INSERT INTO `profile` (`profile_id`, `user_id`, `first_name`, `middle_name`, `last_name`) VALUES
+(9, 10, 'Natan', 'Elio', 'Lellouche'),
+(10, 11, 'Kyle', 'Ozzy', 'Husbands'),
+(11, 12, 'Julien', 'Marks', 'Bernardo');
+
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `publication`
 --
 
-DROP TABLE IF EXISTS `publication`;
 CREATE TABLE `publication` (
   `publication_id` int(11) NOT NULL,
   `profile_id` int(11) NOT NULL,
@@ -68,18 +84,35 @@ CREATE TABLE `publication` (
   `date_time` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `publication`
+--
+
+INSERT INTO `publication` (`publication_id`, `profile_id`, `picture`, `caption`, `date_time`) VALUES
+(34, 9, '63477e0607e2d.png', 'Cat man', '2022-10-12 22:54:00'),
+(35, 10, '63477e3b38ddf.png', 'Pizza time!!!', '2022-10-12 22:55:00'),
+(36, 11, '63477edef0473.png', 'Look at my sonic costume!', '2022-10-05 22:58:00');
+
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `user`
 --
 
-DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
   `user_id` int(11) NOT NULL,
   `username` varchar(50) NOT NULL,
   `password_hash` varchar(72) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`user_id`, `username`, `password_hash`) VALUES
+(10, 'Natan', '$2y$10$3PbRcbZgxB6P6XmGKuS5eeZ.YxhRSg5Uy49L6NT5gU4RRDFMffVSe'),
+(11, 'Kyle', '$2y$10$gyH0rmki89RVRAxC.ealuOfniUe7dKdm9GZ6KXn5MKtfBh69fbqOe'),
+(12, 'Julien', '$2y$10$.X4TfqmpfwJCGUrRkzK3Uut2jUkg6ju9zyMpxL3VPSgyAmwM5PAf2');
 
 --
 -- Indexes for dumped tables
@@ -122,25 +155,25 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `comment`
 --
 ALTER TABLE `comment`
-  MODIFY `comment_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `comment_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `profile`
 --
 ALTER TABLE `profile`
-  MODIFY `profile_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `profile_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `publication`
 --
 ALTER TABLE `publication`
-  MODIFY `publication_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `publication_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- Constraints for dumped tables
